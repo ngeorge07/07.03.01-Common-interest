cc21925b54a4ef0869888769f8592a931223212d
George <geor2335@stud.kea.dk>
2021-09-08 21:38:43 +0200

    basic movement animation

--- scripts/movements.js
+++ scripts/movements.js
@@ -1,6 +1,4 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("_id");
-const url = "https://gnmmd2ndsemester-6f2a.restdb.io/rest/chess/" + id;
+const url = "https://gnmmd2ndsemester-6f2a.restdb.io/rest/chess";
 
 const options = {
   headers: {
@@ -12,11 +10,33 @@ fetch(url, options)
   .then((res) => res.json())
   .then((data) => animations(data));
 
-function animations(detail) {
+function animations(chess) {
   for (let i = 0; i < 25; i++) {
     const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
 
     document.querySelector(".grid").appendChild(clone);
+  }
+
+  const allPieces = document.querySelectorAll("button");
+  console.log(allPieces);
+  for (let y = 0; y < allPieces.length; y++) {
+    allPieces[y].addEventListener("click", showAnimation);
+  }
+
+  const origin = document.querySelector(".grid__item:nth-of-type(13)");
+  let newDiv = document.createElement("div");
+  origin.appendChild(newDiv);
+
+  function showAnimation(e) {
+    console.log(e.target.className);
+
+    if (e.target.className === "pawn-piece") {
+      origin.firstChild.innerHTML = chess[2].codeblk;
+      origin.firstChild.setAttribute("class", "newDiv movePawn");
+    } else if (e.target.className === "queen-piece") {
+      origin.firstChild.innerHTML = chess[0].codeblk;
+      origin.firstChild.setAttribute("class", "newDiv moveQueen");
+    }
   }
 }
